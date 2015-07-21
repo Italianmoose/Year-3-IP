410f27d3ec55bc58a2e97d9b2192570a1ec25a2b
Italianmoose <italianmoose0@gmail.com>
2015-07-21 21:00:23 +0100

    new file:   Combustion Chamber/Chamber v1.mph 	new file:   Lit Review/Collection of combustion rate stuff/NSRDS-NBS67.pdf 	new file:   Lit Review/Collection of combustion rate stuff/link.txt 	new file:   Lit Review/Jumo 004 Investigation Report/eb031311.pdf 	new file:   Lit Review/Jumo 004 Investigation Report/link.txt 	new file:   Lit Review/NASA Code Rules/P10.pdf 	new file:   Lit Review/NASA Code Rules/link.txt 	new file:   Master Map.mm 	new file:   Nozzle/Method-of-Characteristics/Plug/Comsol Analysis/In-progress screengrab.PNG 	modified:   Nozzle/Method-of-Characteristics/Plug/geo.txt 	renamed:    Nozzle/Method-of-Characteristics/Plug/V1 - M4.DXF -> Nozzle/Method-of-Characteristics/Plug/v2/V1 - M4.DXF 	renamed:    Nozzle/Method-of-Characteristics/Plug/V1 - M4.SLDPRT -> Nozzle/Method-of-Characteristics/Plug/v2/V1 - M4.SLDPRT 	renamed:    Nozzle/Method-of-Characteristics/Plug/geo.txt -> Nozzle/Method-of-Characteristics/Plug/v2/geo v2.txt 	new file:   Nozzle/Method-of-Characteristics/Plug/v2/plug nozzle v2.1.mph 	renamed:    Nozzle/Method-of-Characteristics/Plug/plug nozzle v2.mph -> Nozzle/Method-of-Characteristics/Plug/v2/plug nozzle v2.mph 	new file:   Nozzle/Method-of-Characteristics/Plug/v3/geo v3.txt 	new file:   Nozzle/Method-of-Characteristics/Plug/v3/plug nozzle v2.1.mph 	new file:   Nozzle/Method-of-Characteristics/Plug/v3/v3 M1.4.DXF

---
+++ Master Map.mm
@@ -0,0 +1,191 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1436740328238" ID="ID_506927625" MODIFIED="1436742317228" TEXT="Supersonic Ramjet">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1436740458950" ID="ID_1668231302" MODIFIED="1436742317216" POSITION="right" TEXT="Intake">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1436740623016" ID="ID_704714306" MODIFIED="1436742317216" TEXT="Alastair&apos;s Bit">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1436740467578" HGAP="85" ID="ID_824174638" MODIFIED="1436742411358" POSITION="left" TEXT="Combustion Chamber" VSHIFT="11">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1436741242596" ID="ID_1719501475" MODIFIED="1436742317217" TEXT="Design">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1436741249772" ID="ID_1210840489" MODIFIED="1436742317217" TEXT="Rule of Thumb">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1436741269979" ID="ID_355097502" MODIFIED="1436742317217" TEXT="Comsol Basic Turbulent flow">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1436741367547" ID="ID_166083178" MODIFIED="1436742317218" TEXT="Comsol Advanced Turbulent Flow">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1436740476157" HGAP="60" ID="ID_1731135280" MODIFIED="1436742706903" POSITION="right" TEXT="Nozzle" VSHIFT="37">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1436740676522" ID="ID_1582545783" MODIFIED="1436742317218" TEXT="Design">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1436740855456" ID="ID_1527280350" MODIFIED="1436742317218" TEXT="Rule of thumb">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1436740693045" ID="ID_272295070" MODIFIED="1436742317218" TEXT="Method Of Characteristics">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1436740704768" ID="ID_137042245" MODIFIED="1436742317218" TEXT="Comsol Analysis">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1436740789652" ID="ID_1830322619" MODIFIED="1436742317218" TEXT="Full Comsol Optimisation">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1436740880197" ID="ID_52615080" MODIFIED="1436742506619" TEXT="Testing" VSHIFT="24">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1436740888717" ID="ID_1840515898" MODIFIED="1436742317219" TEXT="Comsol">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1436740896278" ID="ID_970441865" MODIFIED="1436742317219" TEXT="Physical Testing">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1436741832848" ID="ID_727152897" MODIFIED="1436742504616" TEXT="Types of Nozzle" VSHIFT="27">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1436741843322" ID="ID_1836458554" MODIFIED="1436742317219" TEXT="Con-Di">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1436741853283" ID="ID_1669621634" MODIFIED="1436742317220" TEXT="Spike">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1436741859654" ID="ID_1939228442" MODIFIED="1436742317220" TEXT="Aerospike"/>
+</node>
+<node COLOR="#990000" CREATED="1436742091433" ID="ID_1437572311" MODIFIED="1436742317220" TEXT="Other">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1436740481520" HGAP="188" ID="ID_313282786" MODIFIED="1436742409690" POSITION="left" TEXT="Telemetry" VSHIFT="-40">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1436741393358" ID="ID_1603770234" MODIFIED="1436742317221" TEXT="Arduino">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1436741154772" ID="ID_1847024144" MODIFIED="1436742317220" TEXT="National Instruments">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1436741410276" ID="ID_1222740246" MODIFIED="1436742317221" TEXT="Raspberry Pi">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1436742972111" ID="ID_725440899" MODIFIED="1436742982065" TEXT="Measurements">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1436742984998" ID="ID_541818118" MODIFIED="1436742989435" TEXT="Thermal">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1436742992498" ID="ID_1122974724" MODIFIED="1436743023378" TEXT="Pressure">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1436743026228" ID="ID_322018702" MODIFIED="1436743029016" TEXT="Static"/>
+<node COLOR="#111111" CREATED="1436743032394" ID="ID_1281023063" MODIFIED="1436743067835" TEXT="Total">
+<node COLOR="#111111" CREATED="1436743081509" ID="ID_1851198513" MODIFIED="1436743095815" TEXT="Pitot tube? Might slash the efficiency"/>
+</node>
+<node COLOR="#111111" CREATED="1436743070721" ID="ID_1212649082" MODIFIED="1436743075155" TEXT="To velocity?"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1436740486693" HGAP="71" ID="ID_177190385" MODIFIED="1436742420549" POSITION="right" TEXT="Control" VSHIFT="47">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1436740928376" HGAP="22" ID="ID_1761708776" MODIFIED="1436742615525" TEXT="Fuel Inlet" VSHIFT="-14">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1436741080303" ID="ID_1050268684" MODIFIED="1436742317221" TEXT="Mechanical">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1436741106210" ID="ID_201990388" MODIFIED="1436742317221" TEXT="Electronic">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1436740942956" ID="ID_1412556067" MODIFIED="1436742317221" TEXT="Igniter">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1436741116733" ID="ID_818468712" MODIFIED="1436742317221" TEXT="Manual">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1436741125996" ID="ID_328114440" MODIFIED="1436742317221" TEXT="Elecronic">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1436742147758" ID="ID_1729597453" MODIFIED="1436742612994" TEXT="Link to telemetry?" VSHIFT="29">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1436740492789" HGAP="212" ID="ID_399627920" MODIFIED="1436742407266" POSITION="left" TEXT="Supply Nozzle" VSHIFT="-66">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1436740610441" ID="ID_1516862878" MODIFIED="1436742317222" TEXT="Alastair&apos;s Bit">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1436740504068" ID="ID_127127976" MODIFIED="1436742418341" POSITION="right" TEXT="Sound Suppression" VSHIFT="43">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1436741552591" ID="ID_391424302" MODIFIED="1436742317222" TEXT="Mount">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1436741557617" ID="ID_1388198466" MODIFIED="1436742317222" TEXT="Supply Nozzle">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="messagebox_warning"/>
+<node COLOR="#990000" CREATED="1436741600191" ID="ID_12645029" MODIFIED="1436742317222" TEXT="Original Limiting Factor - same volume as a major rock gig!">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1436741637353" ID="ID_909940631" MODIFIED="1436742317223" TEXT="Reducing pressure loss and shockwave intensity will be extremely useful">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1436741788678" ID="ID_1494962187" MODIFIED="1436742317226" TEXT="Possible modifications to nozzle to reduce noise - probable loss in performance">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1436741141536" HGAP="89" ID="ID_268161164" MODIFIED="1436742413421" POSITION="left" TEXT="Mounting system" VSHIFT="65">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1436741422145" ID="ID_1640237156" MODIFIED="1436742317227" TEXT="Link to Sound Suppression">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1436741162356" ID="ID_399681287" MODIFIED="1436742317227" POSITION="right" TEXT="Safety">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1436741174895" ID="ID_1520944872" MODIFIED="1436742317227" TEXT="Sound">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1436741180679" ID="ID_986721778" MODIFIED="1436742317228" TEXT="Heat">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1436741185419" ID="ID_1453731536" MODIFIED="1436742317228" TEXT="Emissions">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
